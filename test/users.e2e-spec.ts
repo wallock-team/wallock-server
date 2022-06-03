a290b057171ece7f29981035ba2a4d2324b443a5
Quang Pham <quangpham.245@protonmail.com>
2022-06-03 23:04:47 +0700

    Add test suite for registering new account

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { UsersModule } from './../src/users/users.module';
 
-describe('Users module (e2e)', () => {
+describe('UsersModule (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,7 +15,56 @@ describe('Users module (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/users').expect(200);
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('POST with available email and strong password should receive 201', () => {
+    return request(app.getHttpServer())
+      .post('/users')
+      .send({
+        email: 'foo@example.com',
+        password: 'Password123!',
+      })
+      .expect(201);
+  });
+
+  it('POST without email should receive 400', () => {
+    return request(app.getHttpServer())
+      .post('/users')
+      .send({ password: 'Password123!' })
+      .expect(400);
+  });
+
+  it('POST with taken email should receive 409', () => {
+    request(app.getHttpServer())
+      .post('/users')
+      .send({ email: 'foo@example.com', password: 'Password123!' })
+      .expect(201);
+
+    return request(app.getHttpServer())
+      .post('/users')
+      .send({
+        email: 'foo@example.com',
+        password: 'Password123!',
+      })
+      .expect(409);
+  });
+
+  it('POST without password should receive 400', () => {
+    return request(app.getHttpServer())
+      .post('/users')
+      .send({ email: 'foo@example.com' })
+      .expect(400);
+  });
+
+  it('POST with weak password should receive 400', () => {
+    return request(app.getHttpServer())
+      .post('/users')
+      .send({
+        email: 'foo@example.com',
+        password: 'password',
+      })
+      .expect(400);
   });
 });
