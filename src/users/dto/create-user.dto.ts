c96ba5929ae1ad30a4825d03a624ca2197589ae8
Quang Pham <quangpham.245@protonmail.com>
2022-06-04 00:19:53 +0700

    Update fields and validations of CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsDefined, IsEmail, Matches, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email?: string;
+
+  @IsDefined()
+  @MinLength(8)
+  @Matches(/^.*[A-Z]+.*$/)
+  @Matches(/^.*[a-z]+.*$/)
+  @Matches(/^.*[0-9]+.*$/)
+  @Matches(/^.*[`~!@#$%^&*()-_=+{[\]}\\|;:'",<.>/?]+.*$/)
+  password?: string;
+}
