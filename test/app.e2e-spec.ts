dd82d26b28fef1aad6229bfe8bea7d76fcb58c32
Quang Pham <quangpham.245@protonmail.com>
2022-06-17 19:09:36 +0700

    Update test script to wait for server init

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,36 @@
 import * as Newman from 'newman'
 import * as Pm2 from 'pm2'
+import Axios from 'axios'
+
+it('Postman test', async () => {
+  try {
+    await startTestApp()
+
+    const summary = await runPostmanCollection()
+
+    await stopTestApp()
+
+    if (summary.run.failures.length) {
+      throw new Error(
+        'Postman finished with error(s). View the summary above for details.'
+      )
+    }
+  } catch (error) {
+    throw error
+  }
+}, 30_000)
 
 async function startTestApp() {
+  await startServerProcess()
+  console.info('Server process has started')
+
+  const initStartTime = Date.now()
+  await waitForServerInit()
+  const initTimeInSeconds = (Date.now() - initStartTime) / 1_000
+  console.info(`Server took ${initTimeInSeconds} seconds to init`)
+}
+
+async function startServerProcess() {
   return new Promise<Pm2.Proc>((resolve, reject) => {
     Pm2.start(
       {
@@ -9,10 +38,26 @@ async function startTestApp() {
         script: 'npm run start:dev'
       },
       (err, proc) => {
-        if (err) reject(err)
-        else resolve(proc)
+        if (err) {
+          reject(err)
+        } else {
+          resolve(proc)
+        }
       }
     )
+  })
+}
+
+async function waitForServerInit() {
+  return new Promise<void>((resolve, reject) => {
+    const interval = setInterval(() => {
+      Axios.get('http://localhost:3000')
+        .then(() => {
+          clearInterval(interval)
+          resolve()
+        })
+        .catch(error => {})
+    }, 1_000)
   })
 }
 
@@ -35,26 +80,9 @@ async function stopTestApp() {
   return new Promise<Pm2.Proc>((resolve, reject) => {
     Pm2.delete('app', (err, proc) => {
       Pm2.disconnect()
+      console.info('Server process has stopped')
       if (err) reject(err)
       else resolve(proc)
     })
   })
 }
-
-it('Postman test', async () => {
-  try {
-    await startTestApp()
-
-    const summary = await runPostmanCollection()
-
-    await stopTestApp()
-
-    if (summary.run.failures.length) {
-      throw new Error(
-        'Postman finished with error(s). View the summary above for details.'
-      )
-    }
-  } catch (error) {
-    throw error
-  }
-})
