f805f13a702dca076a66b1db1be33a5c24f5e820
Quang Pham <quangpham.245@protonmail.com>
2022-07-01 18:13:57 +0700

    Run lint on 'users.controller.ts'

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Post, Body, Get, Param } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
-import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger'
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags
+} from '@nestjs/swagger'
 
 @ApiTags('Users API')
 @Controller('users')
@@ -9,17 +15,17 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @ApiOperation({summary:' Receive Data And Create New User'})
+  @ApiOperation({ summary: ' Receive Data And Create New User' })
   @ApiBody({
-    schema:{
+    schema: {
       type: 'object',
       properties: {
-        username:{
+        username: {
           type: 'String',
           example: 'Cuong',
           description: 'This pro is not empty'
         },
-        password:{
+        password: {
           type: 'String',
           example: '123456',
           description: 'This pro is not empty'
@@ -29,35 +35,35 @@ export class UsersController {
   })
   @ApiResponse({
     status: 201,
-    description: 'It will return Username.',
+    description: 'It will return Username.'
   })
   @ApiResponse({
     status: 400,
-    description: 'It will return error message',
+    description: 'It will return error message'
   })
   @ApiResponse({
     status: 409,
-    description: 'It will return message : Email is taken',
+    description: 'It will return message : Email is taken'
   })
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.usersService.create(createUserDto)
   }
 
   @Get()
-  @ApiOperation({summary:' Find All Users'})
+  @ApiOperation({ summary: ' Find All Users' })
   async findALl() {
     return await this.usersService.findAll()
   }
 
   @Get(':username')
-  @ApiOperation({summary:' Find A Specific User'})
+  @ApiOperation({ summary: ' Find A Specific User' })
   @ApiParam({
-    name:'username',
-    type:'String',
-    description:'enter a username',
+    name: 'username',
+    type: 'String',
+    description: 'enter a username',
     required: true
   })
   findOne(@Param('username') username: string) {
-    return this.usersService.findOne(username);
+    return this.usersService.findOne(username)
   }
 }
