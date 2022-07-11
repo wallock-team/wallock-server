7e974aa44d68930836d14fe5487964c68fe1d310
Quang Pham <quangpham.245@protonmail.com>
2022-07-12 00:26:51 +0700

    Update CreateUserDto properties
    As we moving away from using username/password to OIDC, username has became irrelevant. For that, username is removed.
    
    Picture isn't mandatory, so it became optional.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,17 +1,11 @@
 import { IsDefined, IsNotEmpty } from 'class-validator'
 
 export class CreateUserDto {
-
   @IsNotEmpty()
   iss: string
 
   @IsNotEmpty()
   sub: string
 
-  @IsDefined()
-  username: string
-
-  @IsDefined()
-  picture: string
-
+  picture?: string
 }
