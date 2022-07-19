4b44ae6cdd25c371e30f5bb84dcdbe7e89e2325d
Quang Pham <quang-minh.pham-dxc@hpe.com>
2022-07-19 22:12:48 +0700

    Delete SessionSerializer

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common'
 import { PassportModule } from '@nestjs/passport'
-import { SessionSerializer } from './session.serializer'
 import { AuthController } from './auth.controller'
 import { UsersModule } from 'src/users/users.module'
 import { OidcStrategy } from './strategies/oidc/oidc.strategy'
@@ -9,6 +8,6 @@ import { JwtStrategy } from './strategies/jwt/jwt.strategy'
 @Module({
   imports: [PassportModule, UsersModule],
   controllers: [AuthController],
-  providers: [SessionSerializer, OidcStrategy, JwtStrategy]
+  providers: [OidcStrategy, JwtStrategy]
 })
 export class AuthModule {}
