ab9b16e81ddb75fd1f6306612c903f1335ff9ba3
Quang Pham <quang-minh.pham-dxc@hpe.com>
2022-08-11 18:29:42 +0700

    Temporary remove /me in user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,21 +1,11 @@
-import { Controller, Post, Body, Get, Param, Patch, HttpCode, Delete, BadRequestException } from '@nestjs/common'
+import { Controller, Body, Patch, BadRequestException } from '@nestjs/common'
 import { UsersService } from './users.service'
-import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get('/me')
-  async findOne() {
-    //let id = cookie.id.value
-    let id = 2
-    let result = await this.usersService.findOne(id)
-    if (result) return result
-    throw new BadRequestException('Not Found User')
-  }
-  
   @Patch()
   async update(@Body() updateUserDto: UpdateUserDto) {
     //let id = cookie.id.value
