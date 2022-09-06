133fa84e27f7a7dbdd9ddce1652a65d3deb0c477
Quang Pham <quangpham.245@protonmail.com>
2022-09-06 22:43:37 +0700

    Extract building app step into its own file

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,7 @@
-import { ValidationPipe } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import cookieParser from 'cookie-parser'
-import session from 'express-session'
-
-import * as dotenv from 'dotenv'
-dotenv.config()
+import buildApp from './build-app'
 
 async function bootstrap() {
-  const app = (await NestFactory.create(AppModule))
-    .useGlobalPipes(new ValidationPipe({ transform: true }))
-    .use(cookieParser())
-    .use(
-      session({
-        secret: process.env.SESSION_SECRET || '123',
-        resave: false,
-        saveUninitialized: false,
-        rolling: true,
-        cookie: {
-          maxAge: 30 * 60 * 1000,
-          httpOnly: true,
-          secure: process.env.env === 'production'
-        }
-      })
-    )
+  const app = await buildApp()
   await app.listen(3000)
 }
 bootstrap()
