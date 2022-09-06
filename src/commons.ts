9794398716f4d9627316086402ff08424b3e1aa2
Quang Pham <quangpham.245@protonmail.com>
2022-09-06 22:02:42 +0700

    Rename Request to AuthenticatedRequest

--- src/commons.ts
+++ src/commons.ts
@@ -1,6 +1,6 @@
 import { Request as ExpressRequest } from 'express'
 import { User } from './users/entities/user.entity'
 
-export interface Request extends ExpressRequest {
+export interface AuthenticatedRequest extends ExpressRequest {
   user?: User
 }
