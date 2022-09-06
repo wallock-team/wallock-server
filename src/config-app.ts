06df2f86f144b70b90382fe86a6b709effb22020
Quang Pham <quangpham.245@protonmail.com>
2022-09-06 23:35:22 +0700

    Refactor main.ts

--- src/config-app.ts
+++ src/config-app.ts
@@ -2,6 +2,9 @@ import { INestApplication, ValidationPipe } from '@nestjs/common'
 import cookieParser from 'cookie-parser'
 import session from 'express-session'
 
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 export default function configApp(app: INestApplication) {
   return app
     .useGlobalPipes(new ValidationPipe({ transform: true }))
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import buildApp from './build-app'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import configApp from './config-app'
 
 async function bootstrap() {
-  const app = await buildApp()
+  const app = configApp(await NestFactory.create(AppModule))
   await app.listen(3000)
 }
 bootstrap()
